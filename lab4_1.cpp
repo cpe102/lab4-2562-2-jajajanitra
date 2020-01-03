3f2a90cb51a38e2b0ffeee4bad955c0eeec28f2a
lab302-6 <lab302-6@labeng.local>
2020-01-03 16:22:28 +0700

    first

--- lab4_1.cpp
+++ lab4_1.cpp
@@ -1,1 +1,25 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
 
+double functionY(int x){
+    double e = 2.71828;
+    double y;
+    y = (3*x*x*x)+(2*2.71828)+(pow(2,(2*x)+1))+sqrt((x*x)+1);
+    return y;
+}
+
+int main()
+{
+    int x;
+    cout << "Enter x = ";
+    cin >> x;
+
+    functionY(x);
+    cout << "Result y = " << functionY(x);
+    
+
+    return 0;
+
+
+}
