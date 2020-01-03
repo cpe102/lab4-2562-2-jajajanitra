30b0e0adf459a6cc92b3f9891266b15c0024a710
lab302-6 <lab302-6@labeng.local>
2020-01-03 16:23:28 +0700

    first

--- lab4_3.cpp
+++ lab4_3.cpp
@@ -1,1 +1,25 @@
+#include<iostream>
+using namespace std;
 
+int findDivisor(int x){
+    int y =2;
+    while(y<=x){
+        if(x%y == 0){
+            return y;
+        }else{
+            y=y+1;
+        }
+    }
+    return y;
+}
+
+int main()
+{
+    int x;
+    cout << "Enter number = ";
+    cin >> x;
+    cout << "\n" << "Divisor = " << findDivisor(x) ;
+
+    return 0;
+
+}
