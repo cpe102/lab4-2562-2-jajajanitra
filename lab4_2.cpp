9c388f0d67d8f87f6efedb5de7805909ab492b7c
lab302-6 <lab302-6@labeng.local>
2020-01-03 16:23:01 +0700

    first

--- lab4_2.cpp
+++ lab4_2.cpp
@@ -2,11 +2,29 @@
 
 using namespace std;
 
-//Write function findDistance() here
+double findDistance(double u,double a,double t){
+  double s;
+  s = (u*t)+(0.5*t*t*a);
+  return s;
+
+}
 
 int main(){
+  double u;
+  double a;
+  double t;
+  cout << "Enter u =";
+  cin >> u;
+  cout << "\n";
+  cout << "Enter a =";
+  cin >> a;
+  cout << "\n";
+  cout << "Enter t =";
+  cin >> t;
+  cout << "\n";
 
-  //Calling findDistance() using test cases
+  double s;
+  cout << "Distance = " << findDistance(u,a,t);
 
   return 0;
 }
